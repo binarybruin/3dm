487c33e2ec7947eb6d3cc07881416b559d602c9e
Michelle A <binarybruin@server.fake>
2013-12-05 16:00:42 -0500

    play_sound script
    first test

---
+++ play_sound.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class play_sound : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void OnCollisionEnter(Collision box) {
+
+		if (!box.gameObject.audio.isPlaying)
+			box.gameObject.audio.Play();
+	
+	}
+}
