077f7da1118101ebfd28b13341e6b2dcf8010001
binarybruin <michelle.bruin@gmail.com>
2014-03-31 23:21:33 -0400

    PlaySound script
    implement preview audio when mouse over puzzle piece

---
+++ PlaySound.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlaySound : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnMouseEnter() {
+		if (!this.gameObject.audio.isPlaying)
+			this.gameObject.audio.Play();
+		renderer.material.color -= new Color(0.1F, 0, 0);
+	}
+
+	void OnMouseExit() {
+		if (this.gameObject.audio.isPlaying)
+				this.gameObject.audio.Stop ();
+		renderer.material.color += new Color (0.1F, 0, 0);
+	}
+
+}
